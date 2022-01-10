c5c11f94fa22aa68a41fb12318a6c06f7922b3c4
dohyunjo <edan1252@gmail.com>
2022-01-10 11:14:29 +0900

    Initial Project

---
+++ src/main/kotlin/com/banana/telescope/TelescopeApplication.kt
@@ -0,0 +1,11 @@
+package com.banana.telescope
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TelescopeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TelescopeApplication>(*args)
+}
---
+++ src/test/kotlin/com/banana/telescope/TelescopeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.banana.telescope
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TelescopeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
