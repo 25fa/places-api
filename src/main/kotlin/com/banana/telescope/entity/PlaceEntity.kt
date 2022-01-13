2abba629463e5e1676e392ee5f54598480be464b
dohyunjo <edan1252@gmail.com>
2022-01-13 23:21:57 +0900

    Redis TTL change

--- src/main/kotlin/com/banana/telescope/entity/PlaceEntity.kt
+++ src/main/kotlin/com/banana/telescope/entity/PlaceEntity.kt
@@ -4,7 +4,7 @@ import com.banana.telescope.model.PlaceDocument
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 
-@RedisHash(value = "place_cache", timeToLive = 1)
+@RedisHash(value = "place_cache", timeToLive = 180)
 data class PlaceEntity(
     @Id
     val keyword: String,
